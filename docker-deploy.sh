49f797991bde497d1efe3c0c3749c65e6146ba4d
yoseob.lee <yoseob.lee@webtoonscorp.com>
2020-02-16 19:15:21 +0900

    config 변경

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,4 +1,7 @@
 docker login -u YOUR_USERNAME -p YOUR_PASSWORD
 
 docker build -t iosid/test_build:latest .
+docker tag iosid/test_build:0.0.1 iosid/test_build:latest
+
 docker push iosid/test_build:latest
+docker push iosid/test_build:0.0.1
